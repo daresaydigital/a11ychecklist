da08701880e6504a906152bcfd48a4e44e174216
Mohamad Esmaeilpour <mohamad.esmaeilpour@daresay.co>
2020-07-06 19:47:21 +0200

    Using components in md files

---
+++ src/theme/CompletedCheckMark/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import clsx from 'clsx';
+import styles from './styles.module.scss';
+
+export default ({ id }) => {
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  React.useEffect(() => {
+    const item = localStorage.getItem(id);
+    setIsChecked(item && item === "checked");
+  }, []);
+
+  const toggleChecked = () => {
+    if (isChecked) {
+      localStorage.removeItem(id);
+      setIsChecked(false);
+    } else {
+      localStorage.setItem(id, "checked");
+      setIsChecked(true);
+    }
+  };
+
+  return (
+    <div className={clsx(styles.completecheck, { [styles.checked]: isChecked })}>
+      <input
+        type="checkbox"
+        checked={isChecked ? true : false}
+        onChange={toggleChecked}
+      />
+      {isChecked ? "Completed" : "Mark as Completed"}
+    </div>
+  )
+}
