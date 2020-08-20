fed11348db3a9266526e8a8396e017f806c65b88
Mohamad Esmaeilpour <mohamad.esmaeilpour@daresay.co>
2020-08-20 12:41:43 +0200

    Fixed memory leak

--- src/theme/DocPage/index.js
+++ src/theme/DocPage/index.js
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import {MDXProvider} from '@mdx-js/react';
+import { MDXProvider } from '@mdx-js/react';
 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import renderRoutes from '@docusaurus/renderRoutes';
@@ -14,42 +14,46 @@ import Layout from '@theme/Layout';
 import DocSidebar from '@theme/DocSidebar';
 import MDXComponents from '@theme/MDXComponents';
 import NotFound from '@theme/NotFound';
-import {matchPath} from '@docusaurus/router';
+import { matchPath } from '@docusaurus/router';
 
 import styles from './styles.module.css';
 
+var aliasRoutesAdded = false;
+
 function DocPage(props) {
-  const {route: baseRoute, docsMetadata, location} = props;
+  const { route: baseRoute, docsMetadata, location } = props;
   // case-sensitive route such as it is defined in the sidebar
   const currentRoute =
     baseRoute.routes.find((route) => {
       return matchPath(location.pathname, route);
     }) || {};
-  const {permalinkToSidebar, docsSidebars, version} = docsMetadata;
+  const { permalinkToSidebar, docsSidebars, version } = docsMetadata;
   const nodePath = currentRoute.path;
-  const rootPath = nodePath ? nodePath.split("/").slice(0, -1).concat(["index"]).join("/"):nodePath;
+  const rootPath = nodePath ? nodePath.split("/").slice(0, -1).concat(["index"]).join("/") : nodePath;
   const sidebar = permalinkToSidebar[nodePath];
   const sidebarWithFallback = sidebar || permalinkToSidebar[rootPath]
 
   const {
-    siteConfig: {customFields: { aliasRoutes = {}}, themeConfig = {}} = {},
+    siteConfig: { customFields: { aliasRoutes = {} }, themeConfig = {} } = {},
     isClient,
   } = useDocusaurusContext();
 
-  const {sidebarCollapsible = true} = themeConfig;
+  const { sidebarCollapsible = true } = themeConfig;
 
-  const shouldBeAdded = [];
-  baseRoute.routes.forEach(route => {
-    Object.keys(aliasRoutes).forEach(origin => {
-      if (route.path.indexOf(origin) == 0) {
-        aliasRoutes[origin].forEach(aroute => {
-          shouldBeAdded.push(Object.assign({}, route, {path: route.path.replace(origin, aroute)}));
-        });
-      }
+  if (aliasRoutesAdded === false) {
+    const shouldBeAdded = [];
+    baseRoute.routes.forEach(route => {
+      Object.keys(aliasRoutes).forEach(origin => {
+        if (route.path.indexOf(origin) === 0) {
+          aliasRoutes[origin].forEach(aroute => {
+            shouldBeAdded.push(Object.assign({}, route, { path: route.path.replace(origin, aroute) }));
+          });
+        }
+      });
     });
-  });
-
-  shouldBeAdded.forEach(route => baseRoute.routes.push(route));
+    shouldBeAdded.forEach(route => baseRoute.routes.push(route));
+    aliasRoutesAdded = true;
+  }
 
   if (Object.keys(currentRoute).length === 0) {
     return <NotFound {...props} />;
@@ -62,7 +66,7 @@ function DocPage(props) {
           <div className={styles.docSidebarContainer} role="complementary">
             <DocSidebar
               docsSidebars={docsSidebars}
-              path={sidebar ? nodePath:rootPath}
+              path={sidebar ? nodePath : rootPath}
               sidebar={sidebarWithFallback}
               sidebarCollapsible={sidebarCollapsible}
             />
